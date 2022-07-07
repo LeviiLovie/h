6727caf60cb7ea444e5e1327094c28c8ebc49cae
LeviiLovie <leviilovie@gmail.com>
2022-07-07 09:01:42 +0700

    add

--- Main.java
+++ Main.java
@@ -12,14 +12,17 @@ public class Main {
     public static void main(String[] args) {
 		try {ImageIO.write(grabScreen(), "png", new File("screen.png"));} catch (IOException e) {System.out.println("IO exception"+e);}
 		
+		move(250, 250);
+	}
+
+	private static void move(int x, int y) {
 		try {
 			Robot bot = new Robot();
-			bot.mouseMove(50, 50);  
+			bot.mouseMove(x, y);  
 		} catch (AWTException e) {
 			e.printStackTrace();
 		}
 	}
-
 	private static BufferedImage grabScreen() { 
 		try {
 			return new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize())) ;
