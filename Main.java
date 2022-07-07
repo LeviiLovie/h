f9f076c2c0be6dfca447323c8c94878cb0050317
LeviiLovie <leviilovie@gmail.com>
2022-07-07 09:21:59 +0700

    qdd

--- Main.java
+++ Main.java
@@ -7,14 +7,30 @@ import java.io.IOException;
 import java.awt.AWTException;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.awt.event.*;
 
 public class Main {
     public static void main(String[] args) {
 		try {ImageIO.write(grabScreen(), "png", new File("screen.png"));} catch (IOException e) {System.out.println("IO exception"+e);}
 		
-		move(250, 250);
+		// move(50, 30);
+		// press(300, 150);
+		// press(300, 150);
+		// press(50, 30);
+		// move(1400, 175);
+		press(1400, 175);
 	}
 
+	private static void press(int x, int y) {
+		try {
+			Robot bot = new Robot();
+			bot.mouseMove(x, y);
+    		bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+    		bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+	}
 	private static void move(int x, int y) {
 		try {
 			Robot bot = new Robot();
