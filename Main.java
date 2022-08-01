3cc76263da7cf32e73b7ca2bbc1a9cba9587e3be
LeviiLovie <leviilovie@gmail.com>
2022-08-01 10:47:37 +0700

    global commit

--- Main.java
+++ Main.java
@@ -8,18 +8,24 @@ import java.awt.AWTException;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.awt.event.*;
+import java.lang.Thread;
 
 public class Main {
     public static void main(String[] args) {
 		try {ImageIO.write(grabScreen(), "png", new File("screen.png"));} catch (IOException e) {System.out.println("IO exception"+e);}
 		
 		// press(1400, 175);
-		// press(425, 725);
-		press(500, 450);
-		press(700, 450);
+		press2(425, 725);
+
+		try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+		press2(750, 390);
+		try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+		press2(1200, 710);
+		try {Thread.sleep(1000);} catch (InterruptedException e) {e.printStackTrace();}
+		press(1200, 725);
 	}
 
-	private static void press(int x, int y) {
+	private static void press2(int x, int y) {
 		try {
 			Robot bot = new Robot();
 			bot.mouseMove(x, y);
@@ -29,6 +35,16 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
+	private static void press(int x, int y) {
+		try {
+			Robot bot = new Robot();
+			bot.mouseMove(x, y);
+    		bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+	}
+
 	private static void move(int x, int y) {
 		try {
 			Robot bot = new Robot();
